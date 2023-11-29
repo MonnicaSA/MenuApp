1f9ae4af759788daa08d5c7a8743f2fbf0081f54
Monica <jasmin.salinas584@gmail.com>
2023-11-29 11:13:59 +0100

    empezando actividad 2

---
+++ app/src/main/java/com/example/menusapp/SiguienteActivity.kt
@@ -0,0 +1,11 @@
+package com.example.menusapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SiguienteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_siguiente)
+    }
+}
