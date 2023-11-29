93f458a901e7fb6a777c939def5b076163c106b2
Monica <jasmin.salinas584@gmail.com>
2023-11-29 10:52:37 +0100

    Creado 29 nov en clase

---
+++ app/src/main/java/com/example/menusapp/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.menusapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.inputmethod.InputBinding
+import android.widget.Toast
+import com.example.menusapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.actionBar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.action_bar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.actionBarOpt1 -> {
+                Toast
+                    .makeText(
+                        this,
+                        "Opcion 1 ejecutada",
+                        Toast.LENGTH_SHORT
+                    )
+                    .show()
+                true
+            }
+            R.id.actionBarOpt2 -> {
+                Toast
+                    .makeText(
+                        this,
+                        "Opcion 2 ejecutada",
+                        Toast.LENGTH_SHORT
+                    )
+                    .show()
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+
+    }
+}
